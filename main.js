cc61d84a60ccf07c8ba7662b6c4beed48be4999b
arijayjain <98564211+arijayjain@users.noreply.github.com>
2022-12-28 17:08:47 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+
+  Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+  camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+      
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/57mvnjxH3/model.json',modelLoaded);
+
+  function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+function gotResult(error, results) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(results);
+    
+    document.getElementById("result_object_name").innerHTML = results[0].label;
+
+    gesture = results[0].label;
+    
+    toSpeak = "";
+    
+ 
+     if(gesture == "proper")
+    {
+      toSpeak = "Looking good with the mask";
+    }
+    else if(gesture == "no mask")
+    {
+      toSpeak = "You are not allowed";
+    }
+
+    speak();
+  }
+}
+
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    speak_data = toSpeak;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+}
